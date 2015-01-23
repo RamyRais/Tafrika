f04e8e89516c0e3a44adb623564c87945c235b82
Ramy Rais <ramy.rais@gmail.com>
2015-01-23 19:31:45 +0100

    add edit and show fonctionnality to Image

--- src/Tafrika/PostBundle/Controller/ImageController.php
+++ src/Tafrika/PostBundle/Controller/ImageController.php
@@ -11,6 +11,8 @@ namespace Tafrika\PostBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Tafrika\PostBundle\Entity\Image;
 use Tafrika\PostBundle\Form\ImageType;
+use Tafrika\PostBundle\Form\ImageEditType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 
 class ImageController extends Controller{
 
@@ -38,5 +40,46 @@ class ImageController extends Controller{
         return $this->render('TafrikaPostBundle:Image:create.html.twig',array(
             'form'=>$form->createView()
         ));
+    }
+
+    /**
+     * @ParamConverter("image", options={"mapping": {"image_id": "id"}})
+     */
+    public function showImageAction(Image $image){
+        return $this->render('TafrikaPostBundle:Image:show.html.twig',array(
+            'image'=>$image
+        ));
+    }
+
+    /**
+     * @ParamConverter("image", options={"mapping": {"image_id": "id"}})
+     */
+    public function editImageAction(Image $image){
+        if(!$this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')){
+            return $this->redirect($this->generateUrl('status_show', array('status_id'=>$image->getId())));
+        } else{
+            $user = $this->get('security.context')->getToken()->getUser();
+            if($user != $image->getUser()){
+                return $this->redirect($this->generateUrl('status_show', array('status_id'=>$image->getId())));
+
+            }
+        }
+        $form = $this->createForm(new ImageEditType, $image);
+        $request = $this->get('request');
+        if( $request->getMethod() == 'POST'){
+            $form->bind($request);
+            if($form->isValid()){
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($image);
+                $em->flush();
+                return $this->redirect($this->generateUrl('image_show', array('image_id'=>$image->getId())));
+
+            }
+        }
+        return $this->render('TafrikaPostBundle:Image:edit.html.twig',array(
+            'form'=>$form->createView(),
+            'image'=>$image
+        ));
+
     }
 }
