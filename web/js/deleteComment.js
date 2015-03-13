3e34b2ecb6510565457852ecfda43c82e5c7ce2a
Ramy Rais <ramy.rais@gmail.com>
2015-03-13 22:17:03 +0100

    fix bug of missing script import to delete comment

---
+++ web/js/deleteComment.js
@@ -0,0 +1,28 @@
+/**
+ * Created by ramy on 13/03/15.
+ */
+$(document).ready(function(){
+    $('#delete_comment').on('click',function(e){
+        e.preventDefault();
+        var $this = $(this);
+        var $comment_id = $this.attr('content');
+        var $url = $this.attr('href');
+        if($comment_id===''){
+            alert("une erreur est servenu try again later");
+        }else{
+            $.ajax({
+                url: $url,
+                type: 'POST',
+                data: { 'comment_id' : $comment_id },
+                success: function(data) {
+                    $('#comment_list').load(document.URL +  ' #comment_list');
+                    $('#jquery_div').load(document.URL +  ' #jquery_div');
+                },
+                error: function(jq,status,message){
+                    //alert(message);
+                }
+            });
+        }
+
+    });
+});
