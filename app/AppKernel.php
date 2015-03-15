6bb7f58f9f5419eab093b52d5b87466cb66632af
Ramy Rais <ramy.rais@gmail.com>
2015-03-15 12:06:28 +0100

    fix bug of missing new bundle in appKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Tafrika\PostBundle\TafrikaPostBundle(),
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new HWI\Bundle\OAuthBundle\HWIOAuthBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
